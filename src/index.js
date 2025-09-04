83b65fea42ac4e65cbccc66b3cb8eaf06fd9513a
Gregorius Avip <71865513+gregoriusavip@users.noreply.github.com>
2025-09-03 18:09:15 -0700

    Revert "Squashed commit of the following:"
    This reverts commit c8fe5abc191150ae48c2902602d12289a42f9a8e.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 // CSS imports
-import '../node_modules/modern-normalize/modern-normalize.css'
-import "./assets/css/fonts.css"
-import "./assets/css/main.css"
-import "./assets/css/header-footer.css"
+import '../node_modules/modern-normalize/modern-normalize.css';
+import "./assets/css/fonts.css";
+import "./assets/css/main.css";
+import "./assets/css/header-footer.css";
 
 // Script imports
-import { main as mainSection } from "./scripts/mainSection"
-import { home } from "./scripts/home"
-
-// Append elements to the home section
-const homeSection = document.querySelector("main section#home-section div.container");
-homeSection.appendChild(home.heading);
-homeSection.appendChild(home.headingDescription);
-homeSection.appendChild(home.divCard);
-homeSection.appendChild(home.asideCard);
+import { main as mainSection } from "./scripts/mainSection";
